a5f254a1b9813af94b02ffa027c0502f707bb311
AiAuteurs <153168008+AiAuteurs@users.noreply.github.com>
2026-05-04 06:37:31 -0700

    Update issues.js

--- data/issues.js
+++ data/issues.js
@@ -644,3 +644,7 @@ export function getIssueById(id) {
   return Object.values(issues).find((i) => i.id === Number(id)) || null;
 }
 
+
+export function getAllIssues() {
+  return Object.values(issues)
+}
